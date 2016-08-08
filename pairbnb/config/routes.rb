23afb3c783618796c59f7783ed7a445b18b354b6
m3y2x1 <m3y2x1@gmail.com>
2016-08-08 12:58:12 +0800

    clearance added

--- pairbnb/config/routes.rb
+++ pairbnb/config/routes.rb
@@ -1,4 +1,31 @@
+
+
+#*** rails g clearance:routes show all these default Clearance routes
+
 Rails.application.routes.draw do
+
+  root 'welcome#index'
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+  resources :users, controller: "users", only: :show
+
+  resources :users, controller: "clearance/users", only: [:create] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:create, :edit, :update]
+  end
+
+  get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  get "/sign_up" => "clearance/users#new", as: "sign_up"
+  #*** end of default routes of Clearance
+  get 'welcome/index'
+
+  get 'users/show'
+
+
+get "/auth/:provider/callback" => "sessions#create_from_omniauth"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
